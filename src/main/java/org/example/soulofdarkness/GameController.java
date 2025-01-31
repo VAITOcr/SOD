b03560a06510e0c4a1e710c698936f9c3810a57a
VAITOcr <kevinjec44@gmail.com>
2025-01-31 16:18:29 -0500

    Button exit added

--- src/main/java/org/example/soulofdarkness/GameController.java
+++ src/main/java/org/example/soulofdarkness/GameController.java
@@ -11,4 +11,35 @@ import javafx.stage.Stage;
 
 public class GameController {
 
+    
+    // private static final int MAZE_WIDTH = 21;
+    // private static final int MAZE_HEIGHT = 21;
+
+    /**
+     * @Override
+     *           public void start(Stage primaryStage) {
+     *           GameView gameView = new GameView(MAZE_WIDTH, MAZE_HEIGHT);
+     *           InputHandler inputHandler = new InputHandler(gameView);
+     * 
+     *           Scene scene = new Scene(new StackPane(gameView));
+     *           scene.setOnKeyPressed(inputHandler::handleKeyPress);
+     * 
+     *           primaryStage.setTitle("Souls of Darkness - Rogue-Like");
+     *           primaryStage.setScene(scene);
+     *           primaryStage.show();
+     * 
+     *           // Boucle pour faire bouger les ennemis toutes les 500ms
+     *           new AnimationTimer() {
+     *           private long lastUpdate = 0;
+     * 
+     * @Override
+     *           public void handle(long now) {
+     *           if (now - lastUpdate > 500_000_000) { // 500 ms
+     *           gameView.moveEnemies();
+     *           lastUpdate = now;
+     *           }
+     *           }
+     *           }.start();
+     *           }
+     **/
 }
--- src/main/java/org/example/soulofdarkness/GameMainMenuController.java
+++ src/main/java/org/example/soulofdarkness/GameMainMenuController.java
@@ -1,22 +1,30 @@
 package org.example.soulofdarkness;
 
-
+import javafx.animation.PauseTransition;
+import javafx.animation.ScaleTransition;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
-
+import javafx.util.Duration;
 
 public class GameMainMenuController extends Application {
 
     @FXML
     private Pane GifMainMenu;
+
+    @FXML
+    private Button StartButton;
+
+    @FXML
+    private Button ExitButton;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -30,7 +38,7 @@ public class GameMainMenuController extends Application {
         gifView.setFitWidth(894);
         GifMainMenu.getChildren().add(gifView);
 
-        //Musique de fond
+        // Musique de fond
         String ostPath = getClass().getResource("/sound/MainMenuOST.mp3").toString();
         Media media = new Media(ostPath);
         MediaPlayer mediaPlayer = new MediaPlayer(media);
@@ -38,47 +46,39 @@ public class GameMainMenuController extends Application {
         mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
         mediaPlayer.setVolume(0.5);
 
-        
-        
-
         Scene scene = new Scene(root);
         primaryStage.setTitle("Souls of Darkness - Rogue-Like");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+    @FXML
+    private void closeGame() {
+        ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(50), ExitButton);
+        scaleTransition.setFromX(1);
+        scaleTransition.setFromY(1);
+        scaleTransition.setToX(0.8);
+        scaleTransition.setToY(0.8);
+        scaleTransition.setAutoReverse(true);
+        scaleTransition.setCycleCount(2);
+        PauseTransition pauseTransition = new PauseTransition(Duration.millis(100));
+        pauseTransition.setOnFinished(event -> {
+            System.exit(0);
+        });
+        pauseTransition.play();
+        scaleTransition.play();
+
+    }
+
+    @FXML
+    private void startGame() throws Exception {
+        Stage stage = new Stage();
+        stage.setTitle("Souls of Darkness - Rogue-Like");
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/fxml/GameView.fxml"))));
+        stage.show();
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
 }
-
-// private static final int MAZE_WIDTH = 21;
-// private static final int MAZE_HEIGHT = 21;
-
-/**
- * @Override
- *           public void start(Stage primaryStage) {
- *           GameView gameView = new GameView(MAZE_WIDTH, MAZE_HEIGHT);
- *           InputHandler inputHandler = new InputHandler(gameView);
- * 
- *           Scene scene = new Scene(new StackPane(gameView));
- *           scene.setOnKeyPressed(inputHandler::handleKeyPress);
- * 
- *           primaryStage.setTitle("Souls of Darkness - Rogue-Like");
- *           primaryStage.setScene(scene);
- *           primaryStage.show();
- * 
- *           // Boucle pour faire bouger les ennemis toutes les 500ms
- *           new AnimationTimer() {
- *           private long lastUpdate = 0;
- * 
- * @Override
- *           public void handle(long now) {
- *           if (now - lastUpdate > 500_000_000) { // 500 ms
- *           gameView.moveEnemies();
- *           lastUpdate = now;
- *           }
- *           }
- *           }.start();
- *           }
- **/
