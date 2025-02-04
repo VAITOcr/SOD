5d627e9dbebe3278e5e134857db7ca9eb2968ece
VAITOcr <kevinjec44@gmail.com>
2025-02-03 19:05:59 -0500

    added assets for floor and wall

--- src/main/java/org/example/soulofdarkness/Ui/GameView.java
+++ src/main/java/org/example/soulofdarkness/Ui/GameView.java
@@ -6,17 +6,27 @@ import org.example.soulofdarkness.model.Player;
 
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.media.Media;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class GameView extends Canvas {
     private static final int TILE_SIZE = 30; // Taille d'une case
     private MazeGenerator mazeGenerator;
     private int[][] maze;
-    private Player player = new Player(1, 1, 100, 100, 0, 1, 10, 5, 5, 100);
+    private Player player = new Player(1, 1, 100, 100, 0, 1, 10, 5, 5, 100,
+            new Image(getClass().getResource("/assets/Player.png").toString()));
+
+    // Images for assets
+    private Image wallImage = new Image(getClass().getResource("/assets/Wall1.png").toString());
+    private Image floorImage = new Image(getClass().getResource("/assets/Floor1.jpg").toString());
+    
+
     // private int playerX = 1, playerY = 1; // Position du joueur
     private List<Enemy> enemies = new ArrayList<>(); // Liste des ennemis
     private Canvas canvas;
@@ -26,12 +36,13 @@ public class GameView extends Canvas {
         this.canvas.setWidth(width * TILE_SIZE);
         this.canvas.setHeight(height * TILE_SIZE);
         generateNewMaze(width, height);
+        moveEnemies();
     }
 
     public void generateNewMaze(int width, int height) {
         mazeGenerator = new MazeGenerator(width, height);
         maze = mazeGenerator.getMaze();
-        spawnEnemies(3); // Générer 3 ennemis
+        spawnEnemies(5); // Générer 3 ennemis
         drawMaze();
     }
 
@@ -47,7 +58,7 @@ public class GameView extends Canvas {
         System.out.println("Position du joueur : (" + player.getX() + ", " + player.getY() + ")");
         // Mettre à jour la position du joueur et des ennemis
         checkEnemyCollision();
-        drawMaze();
+        moveEnemies();
     }
 
     public void moveEnemies() {
@@ -80,21 +91,28 @@ public class GameView extends Canvas {
 
         for (int y = 0; y < maze.length; y++) {
             for (int x = 0; x < maze[0].length; x++) {
-                gc.setFill(maze[y][x] == 0 ? Color.BLACK : Color.WHITE);
-                gc.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                if (maze[y][x] == 0) {
+                    gc.drawImage(floorImage, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                } else if (maze[y][x] == 1) {
+                    gc.drawImage(wallImage, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                }
             }
         }
 
         // Dessiner le joueur en bleu
-        gc.setFill(Color.BLUE);
-        gc.fillOval(player.getX() * TILE_SIZE + TILE_SIZE / 4, player.getY() * TILE_SIZE + TILE_SIZE / 4, TILE_SIZE / 2,
-                TILE_SIZE / 2);
+
+        gc.drawImage(player.getImagePlayer(),
+                player.getX() * TILE_SIZE,
+                player.getY() * TILE_SIZE,
+                TILE_SIZE, TILE_SIZE);
 
         // Dessiner les ennemis en rouge
-        gc.setFill(Color.RED);
+        Random random = new Random();
         for (Enemy enemy : enemies) {
-            gc.fillOval(enemy.getX() * TILE_SIZE + TILE_SIZE / 4, enemy.getY() * TILE_SIZE + TILE_SIZE / 4,
-                    TILE_SIZE / 2, TILE_SIZE / 2);
+            gc.drawImage(enemy.getEnemyImage(),
+                    enemy.getX() * TILE_SIZE,
+                    enemy.getY() * TILE_SIZE,
+                    TILE_SIZE, TILE_SIZE);
         }
     }
 }
